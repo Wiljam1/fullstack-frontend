cfe9b1d091cd7326d6328caf1f5d16d4469ca286
Wiljam Wilmi <wiljam.wilmi@hotmail.com>
2024-01-06 16:55:47 +0100

    new client secret

--- src/users/Login.js
+++ src/users/Login.js
@@ -23,7 +23,7 @@ export default function Login() {
         const tokenUrl = 'https://keycloak.app.cloud.cbh.kth.se/realms/patient-keycloak/protocol/openid-connect/token';
         const client_id = 'spring-auth';
         const grant_type = 'password';
-        const client_secret = 'Cy5NXaZEkQaXm7TjI6Uq1CBTmNz6ainJ'; 
+        const client_secret = '9DjdN3bEZeC4v1hV2wXczDf0ycVLlWOc'; 
     
         const params = new URLSearchParams();
         params.append('client_id', client_id);
